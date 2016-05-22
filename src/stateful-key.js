562258bfc1a17434a2d49676ba636d0f2b5c9105
Roman Pushkin <roman@breakthrough.com>
2016-05-21 21:22:55 -0700

    #30 Update StatefulKey.toString() doc

--- src/stateful-key.js
+++ src/stateful-key.js
@@ -23,6 +23,20 @@ export default class StatefulKey extends ValidatedStatefulKey {
    * @throws {ArgumentError} throw error when:
    * - options.platformType not specified or not supported
    * - options.platformId not specified
+   */
+  constructor(options) {
+    super(options);
+    objectAssign(this, options);
+  }
+
+  /**
+   * Converts object to string representation.
+   *
+   * @author Roman Pushkin (roman.pushkin@gmail.com)
+   * @date 2016-05-22
+   * @version 1.1
+   * @since 0.1.0
+   * @return {string} String representation of object
    * @example
    * const k = new StatefulKey('telegram', '31337', '4566bd48-d369-4594-aa1e-fb5dae1acf43');
    * k.toString(); // => telegram_31337_4566bd48-d369-4594-aa1e-fb5dae1acf43
@@ -30,11 +44,6 @@ export default class StatefulKey extends ValidatedStatefulKey {
    * const k = new StatefulKey('cli', '1');
    * k.toString(); // => cli_1
    */
-  constructor(options) {
-    super(options);
-    objectAssign(this, options);
-  }
-
   toString() {
     let result = `${this.platformType}_${this.platformId}`;
     if (this.guid) {
