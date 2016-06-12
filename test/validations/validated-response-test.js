ed7ad51e1ba88f75e889bee2565b0c7c2efcef63
Roman Pushkin <roman.pushkin@gmail.com>
2016-06-12 14:58:44 -0700

    #68 Refactor response and validation

--- test/validations/validated-response-test.js
+++ test/validations/validated-response-test.js
@@ -3,28 +3,7 @@ import test from 'ava';
 import { mix } from 'mixwith';
 import Mixin from '../../src/validations/validated-response';
 
-class ValidatedResponse extends mix(Object).with(Mixin) {
-}
-
-test.cb('should throw error when parameters not specified', t => {
-  const err = 'constructor parameters not specified';
-
-  t.throws(() => {
-    new ValidatedResponse();
-  }, err);
-
-  t.end();
-});
-
-test.cb('should throw error when "type" parameter not specified', t => {
-  const err = 'type parameter not specified';
-
-  t.throws(() => {
-    new ValidatedResponse({ foo: 123 });
-  }, err);
-
-  t.end();
-});
+class ValidatedResponse extends mix(Object).with(Mixin) {}
 
 test.cb('should throw error when type not supported', t => {
   const err = 'response with type "foo" not supported';
