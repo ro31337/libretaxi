acd1e3167ac82737e74bff402faaf7b69911f1ef
Roman Pushkin <roman@breakthrough.com>
2016-07-09 09:31:21 -0700

    #90 Add action

---
+++ test/action-test.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-new */
+import test from 'ava';
+import Action from '../src/action';
+import checkNotNullTest from './helpers/check-not-null.js';
+
+checkNotNullTest('i18n', (args) => { new Action(args); });
+
+test('can be constructed with default parameters', t => {
+  new Action({ i18n: {} });
+  t.pass();
+});
+
+test.cb('should throw error when for missing methods', t => {
+  const err = 'not implemented';
+  const action = new Action({ i18n: {} });
+
+  t.throws(() => { action.post(); }, err);
+  t.throws(() => { action.text(); }, err);
+  t.throws(() => { action.get(); }, err);
+  t.throws(() => { action.id(); }, err);
+
+  t.end();
+});
+
+test('should set state if not specified', t => {
+  const action = new Action({ i18n: {} });
+  t.truthy(action.state);
+  t.pass();
+});
+
+test('should set state if specified', t => {
+  const action = new Action({ i18n: {}, state: { a: 1 } });
+  t.is(action.state.a, 1);
+  t.pass();
+});
