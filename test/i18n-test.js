bfbd0545bf41ba8c9a910db334603763f7263bd6
Roman Pushkin <roman.pushkin@gmail.com>
2016-07-08 20:12:25 -0700

    #86 Migrate eslint to v.3 (#87)

--- test/i18n-test.js
+++ test/i18n-test.js
@@ -9,13 +9,13 @@ test.cb('locales should have all keys from en.json, except *_desc', t => {
   t.plan(NUM_OF_LOCALIZATIONS - 1);
 
   const walker = walk.walk('../locales', { followLinks: false });
-  const en = require('../locales/en.json');
+  const en = require('../locales/en.json'); // eslint-disable-line global-require
 
   // walk through each file, except en.json
   walker.on('file', (root, stat, next) => {
     if (stat.name !== 'en.json') {
       const path = `${root}/${stat.name}`;
-      const xx = require(path);
+      const xx = require(path); // eslint-disable-line global-require
 
       for (const key of Object.keys(xx)) {
         if (xx[key].length === 0) t.fail(`Value length for key '${key}' in ${stat.name} is 0`);
@@ -39,7 +39,7 @@ test.cb('locales should have all keys from en.json, except *_desc', t => {
 });
 
 test('en.json should have corresponding *_desc keys', t => {
-  const en = require('../locales/en.json');
+  const en = require('../locales/en.json'); // eslint-disable-line global-require
 
   for (const key of Object.keys(en)) {
     if (key.endsWith('_desc')) continue;
@@ -60,7 +60,7 @@ test.cb('locales except en.json should not have keys ending with _desc', t => {
   walker.on('file', (root, stat, next) => {
     if (stat.name !== 'en.json') {
       const path = `${root}/${stat.name}`;
-      const xx = require(path);
+      const xx = require(path); // eslint-disable-line global-require
 
       for (const key of Object.keys(xx)) {
         if (key.endsWith('_desc')) {
