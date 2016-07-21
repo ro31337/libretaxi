41797e9165f75037cfa56aeb4f6f88c93bd5dd2f
Roman Pushkin <roman@breakthrough.com>
2016-07-21 13:39:04 -0700

    #129 Add select locale menu action

--- test/menu/select-locale-test.js
+++ test/menu/select-locale-test.js
@@ -1,7 +1,29 @@
-/* eslint-disable no-new */
+/* eslint-disable no-new, no-unused-vars */
 import test from 'ava';
+import routes from '../../src/routes'; // to aviod curcular dependencies
 import SelectLocale from '../../src/menu/select-locale';
 
-test('should be stubbed for now', t => {
-  t.is(new SelectLocale({ i18n: {} }).foo(), 'bar');
+test('can be constructed with default parameters', t => {
+  new SelectLocale({ i18n: {} });
+  t.pass();
+});
+
+test('should return composite response on get', t => {
+  const action = new SelectLocale({ i18n: {} });
+  const response = action.get();
+  t.is(response.type, 'composite');
+  t.is(response.responses[0].type, 'text');
+  t.is(response.responses[0].message, 'Select your language:');
+  t.is(response.responses[1].type, 'options');
+  t.is(response.responses[1].rows[0][0].value, 'en');
+  t.is(response.responses[1].rows[0][1].value, 'ru');
+});
+
+test('should return composite response on post', t => {
+  const action = new SelectLocale({ i18n: {} });
+  const response = action.post('en');
+  t.is(response.type, 'composite');
+  t.is(response.responses[0].type, 'user-state');
+  t.is(response.responses[1].type, 'redirect');
+  t.is(response.responses[1].path, 'default');
 });
