73cb86070ad3e6d3a2c84471c6e912a30f1b3648
Roman Pushkin <roman.pushkin@gmail.com>
2016-07-21 13:41:20 -0700

    #129 Add select locale menu action (#130)
    * #129 Add select locale menu action
    
    * #129 Fix typo

--- src/menu/select-locale.js
+++ src/menu/select-locale.js
@@ -1,12 +1,16 @@
 import Action from '../action';
 import objectAssign from 'object-assign';
-
+import OptionsResponse from '../responses/options-response';
+import CompositeResponse from '../responses/composite-response';
+import SelectLocaleResponse from '../responses/select-locale-response';
+import TextResponse from '../responses/text-response';
+import RedirectResponse from '../responses/redirect-response';
 /**
- * Select locale menu action, stubbed for now.
+ * Select locale menu action.
  *
  * @author Roman Pushkin (roman.pushkin@gmail.com)
  * @date 2016-05-26
- * @version 1.2
+ * @version 1.3
  * @since 0.1.0
  */
 export default class SelectLocale extends Action {
@@ -19,11 +23,30 @@ export default class SelectLocale extends Action {
   }
 
   /**
-   * Stub method
+   * Returns greeting text and list of available languages.
    *
-   * @return {string} Returns `bar`
+   * @return {CompositeResponse} Returns instance of {@link CompositeResponse}
+   * which contains {@link TextResponse} and {@link OptionsResponse}.
    */
-  foo() {
-    return 'bar';
+  get() {
+    return new CompositeResponse()
+      .add(new TextResponse({ message: 'Select your language:' }))
+      .add(new OptionsResponse({
+        rows: [
+          [{ label: 'English', value: 'en' }, { label: 'Русский', value: 'ru' }],
+        ],
+      }));
+  }
+
+  /**
+   * Sets selected locale and redirects.
+   *
+   * @return {CompositeResponse} Returns instance of {@link CompositeResponse}
+   * which contains {@link SelectLocaleResponse}, and {@link RedirectResponse}.
+   */
+  post(value) {
+    return new CompositeResponse()
+      .add(new SelectLocaleResponse({ locale: value }))
+      .add(new RedirectResponse({ path: 'default' }));
   }
 }
