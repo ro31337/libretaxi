d64c57f0b3d06c64b883f427ac58d7a13ceb15e1
Roman Pushkin <roman@breakthrough.com>
2016-07-21 23:43:04 -0700

    #131 Add app loop

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
 import Log from './log';
 import './init';
+import User from './user';
+import ActionFactory from './action-factory';
+import ResponseHandlerFactory from './factories/response-handler-factory';
 
 const log = new Log();
 log.debug(__('app.welcome_banner'));
+
+new User({ platformType: 'cli', platformId: '1' }).load().then((user) => {
+  const f = (arg) => {
+    // console.log(`Current user state:`);
+    // console.dir(user.state);
+
+    const action = ActionFactory.fromMenuLocation(user);
+    const response = action.call(arg);
+    const handler = ResponseHandlerFactory.getHandler({ response, user });
+
+    handler.call((value) => {
+      f(value);
+    });
+  };
+  f();
+})
+.catch((err) => {
+  console.dir(err); // eslint-disable-line no-console
+});
