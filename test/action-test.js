bc8c677452e93402dba21ca15d257f54f9300673
Roman Pushkin <roman@breakthrough.com>
2016-07-24 15:10:22 -0700

    #136 Add action translation helper test

--- test/action-test.js
+++ test/action-test.js
@@ -2,17 +2,18 @@
 import test from 'ava';
 import Action from '../src/action';
 import checkNotNullTest from './helpers/check-not-null.js';
+import { i18n } from './spec-support';
 
 checkNotNullTest(['i18n', 'type'], (args) => { new Action(args); });
 
 test('can be constructed with default parameters', t => {
-  new Action({ i18n: {}, type: 'foo' });
+  new Action({ i18n, type: 'foo' });
   t.pass();
 });
 
 test.cb('should throw error when for missing methods', t => {
   const err = 'not implemented';
-  const action = new Action({ i18n: {}, type: 'foo' });
+  const action = new Action({ i18n, type: 'foo' });
 
   t.throws(() => { action.post(); }, err);
   t.throws(() => { action.text(); }, err);
@@ -23,19 +24,19 @@ test.cb('should throw error when for missing methods', t => {
 });
 
 test('should set state if not specified', t => {
-  const action = new Action({ i18n: {}, type: 'foo' });
+  const action = new Action({ i18n, type: 'foo' });
   t.truthy(action.state);
   t.pass();
 });
 
 test('should set state if specified', t => {
-  const action = new Action({ i18n: {}, state: { a: 1 }, type: 'foo' });
+  const action = new Action({ i18n, state: { a: 1 }, type: 'foo' });
   t.is(action.state.a, 1);
   t.pass();
 });
 
 test.cb('should call get on `call` when arg is not provided', t => {
-  const action = new Action({ i18n: {}, type: 'foo' });
+  const action = new Action({ i18n, type: 'foo' });
   action.get = () => {
     t.pass();
     t.end();
@@ -44,10 +45,15 @@ test.cb('should call get on `call` when arg is not provided', t => {
 });
 
 test.cb('should call post on `call` when arg is provided', t => {
-  const action = new Action({ i18n: {}, type: 'foo' });
+  const action = new Action({ i18n, type: 'foo' });
   action.post = (arg) => {
     t.is(arg, 123);
     t.end();
   };
   action.call(123);
+});
+
+test('translation helper method should work', t => {
+  const action = new Action({ i18n, type: 'select-user-type' });
+  t.is(action.t('who_you_are'), 'Who you are? (select for now, you can change it later)');
 });
