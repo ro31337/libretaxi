03a1bf9ab71edcd095cbdb447119be341aef3c32
Roman Pushkin <roman.pushkin@gmail.com>
2016-07-28 16:31:16 -0700

    #150 Add passenger index action (#155)

---
+++ src/menu/passenger/index.js
@@ -0,0 +1,73 @@
+import Action from '../../action';
+import objectAssign from 'object-assign';
+import OptionsResponse from '../../responses/options-response';
+import CompositeResponse from '../../responses/composite-response';
+import UserStateResponse from '../../responses/user-state-response';
+import TextResponse from '../../responses/text-response';
+import RedirectResponse from '../../responses/redirect-response';
+/**
+ * Passenger index: asks to choose taxi type.
+ *
+ * @author Roman Pushkin (roman.pushkin@gmail.com)
+ * @date 2016-07-28
+ * @version 1.1
+ * @since 0.1.0
+ */
+export default class PassengerIndex extends Action {
+
+  /**
+   * Constructor.
+   */
+  constructor(options) {
+    super(objectAssign({ type: 'passenger-index' }, options));
+  }
+
+  /**
+   * Returns list of available taxi types (bike and car) + settings button.
+   *
+   * @return {CompositeResponse} Returns instance of {@link CompositeResponse}
+   * which contains {@link TextResponse} and {@link OptionsResponse}.
+   */
+  get() {
+    return new CompositeResponse()
+      .add(new TextResponse({ message: this.t('choose_taxi_type') }))
+      .add(new OptionsResponse({
+        rows: [
+          [
+            { label: this.t('bike'), value: 'bike' },
+            { label: this.t('car'), value: 'car' },
+            { label: this.t('s'), value: 'settings' },
+          ],
+        ],
+      }));
+  }
+
+  /**
+   * Saves `taxiType` to user's state and redirects to request location action.
+   * When settings is picked, redirects to settings.
+   *
+   * @return {CompositeResponse} Returns instance of {@link CompositeResponse}
+   * which is one of the following combinations:
+   * - {@link TextResponse} + {@link UserStateResponse} + {@link RedirectResponse}
+   * - {@link RedirectResponse}
+   */
+  post(value) {
+    const response = new CompositeResponse();
+
+    switch (value) {
+      case 'bike':
+      case 'car':
+        response.add(new TextResponse({ message: '👌 OK!' }));
+        response.add(new UserStateResponse({ taxiType: value }));
+        response.add(new RedirectResponse({ path: 'passenger-request-location' }));
+        break;
+      case 'settings':
+        response.add(new RedirectResponse({ path: 'settings' }));
+        break;
+      default:
+        throw new Error(`unsupported choice '${value}'`);
+    }
+
+    return response;
+  }
+}
---
+++ test/menu/passenger/index-test.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-new, no-unused-vars */
+import test from 'ava';
+import routes from '../../../src/routes'; // to aviod circular dependencies
+import PassengerIndex from '../../../src/menu/passenger/index';
+import { i18n } from '../../spec-support';
+
+const user = {};
+
+test('can be constructed with default parameters', t => {
+  new PassengerIndex({ i18n, user });
+  t.pass();
+});
+
+test('should return composite response on get', t => {
+  const action = new PassengerIndex({ i18n, user });
+  const response = action.get();
+  t.is(response.type, 'composite');
+  t.is(response.responses[0].type, 'text');
+  t.is(response.responses[0].message, i18n.__('passenger-index.choose_taxi_type'));
+  t.is(response.responses[1].type, 'options');
+  t.is(response.responses[1].rows[0][0].value, 'bike');
+  t.is(response.responses[1].rows[0][1].value, 'car');
+  t.is(response.responses[1].rows[0][2].value, 'settings');
+  t.is(response.responses[1].rows[0][0].label, i18n.__('passenger-index.bike'));
+  t.is(response.responses[1].rows[0][1].label, i18n.__('passenger-index.car'));
+  t.is(response.responses[1].rows[0][2].label, i18n.__('passenger-index.s'));
+});
+
+test('should return composite response on post for bike and car', t => {
+  const reactions = ['bike', 'car'];
+  const action = new PassengerIndex({ i18n, user });
+
+  for (const reaction of reactions) {
+    const response = action.post(reaction);
+    t.is(response.type, 'composite');
+    t.is(response.responses[0].type, 'text');
+    t.is(response.responses[0].message, '👌 OK!');
+    t.is(response.responses[1].type, 'user-state');
+    t.is(response.responses[1].state.taxiType, reaction);
+    t.is(response.responses[2].type, 'redirect');
+    t.is(response.responses[2].path, 'passenger-request-location');
+  }
+});
+
+test('should return different composite response on post for settings', t => {
+  const action = new PassengerIndex({ i18n, user });
+  const response = action.post('settings');
+  t.is(response.type, 'composite');
+  t.is(response.responses[0].type, 'redirect');
+  t.is(response.responses[0].path, 'settings');
+});
+
+test.cb('should throw error on post for unknown reaction', t => {
+  const action = new PassengerIndex({ i18n, user });
+  const err = 'unsupported choice \'whatever\'';
+
+  t.plan(1);
+  t.throws(() => { action.post('whatever'); }, err);
+  t.end();
+});
