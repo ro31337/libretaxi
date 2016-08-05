4bd3f3f029d314d25d6e4f4ebef19571d2ad25f2
Roman Pushkin <roman.pushkin@gmail.com>
2016-08-05 16:00:58 -0700

    #171 Improve update location response (#172)

--- src/responses/update-location-response.js
+++ src/responses/update-location-response.js
@@ -9,27 +9,24 @@ import checkNotNull from '../validations/check-not-null.js';
  * @author Roman Pushkin (roman.pushkin@gmail.com)
  * @extends {Response}
  * @date 2016-08-05
- * @version 1.1
+ * @version 1.2
  * @since 0.1.0
  */
 export default class UpdateLocationResponse extends
-  mix(Response).with(checkNotNull(['userKey', 'location'])) {
+  mix(Response).with(checkNotNull('location')) {
   /**
    * Constructor.
    *
    * @type {Object}
-   * @param {string} options.userKey - user key in the database. For example, `cli_1`
    * @param {string} options.location - user's location, array with two decimal coordinates.
    * For example: `[37.421955, -122.084058]` (location of GooglePlex in MTV)
    * @throws {ArgumentError} throw error when message parameter(s) not specified.
    * @example
-   * r = new UpdateLocationResponse({userKey: 'cli_1', location: [37.421955, -122.084058]});
-   * console.log(r.userKey); // prints "cli_1"
+   * r = new UpdateLocationResponse({ location: [37.421955, -122.084058] });
    * console.log(r.location); // prints "[37.421955, -122.084058]"
    */
   constructor(options) {
     super(Object.assign({ type: 'update-location' }, options));
-    this.userKey = options.userKey;
     this.location = options.location;
   }
 }
--- test/responses/update-location-response-test.js
+++ test/responses/update-location-response-test.js
@@ -3,24 +3,22 @@ import test from 'ava';
 import UpdateLocationResponse from '../../src/responses/update-location-response';
 import checkNotNullTest from '../helpers/check-not-null.js';
 
-checkNotNullTest(['userKey', 'location'], (args) => { new UpdateLocationResponse(args); });
+checkNotNullTest('location', (args) => { new UpdateLocationResponse(args); });
 
 const location = [37.421955, -122.084058];
-const userKey = 'cli_1';
 
 test('can be constructed with default parameters', t => {
-  new UpdateLocationResponse({ userKey, location });
+  new UpdateLocationResponse({ location });
   t.pass();
 });
 
 test('should have type', t => {
-  const r = new UpdateLocationResponse({ userKey, location });
+  const r = new UpdateLocationResponse({ location });
   t.is(r.type, 'update-location');
 });
 
-test('should save properties', t => {
-  const r = new UpdateLocationResponse({ userKey, location });
-  t.is(r.userKey, 'cli_1');
+test('should save location property', t => {
+  const r = new UpdateLocationResponse({ location });
   t.is(r.location[0], 37.421955);
   t.is(r.location[1], -122.084058);
 });
