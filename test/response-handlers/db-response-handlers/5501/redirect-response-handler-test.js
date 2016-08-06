890cfbff2621c24956ce1f121cee2c864218033e
Roman Pushkin <roman@breakthrough.com>
2016-08-06 10:56:15 -0700

    #119 Split and reorganize handler db tests

---
+++ test/response-handlers/db-response-handlers/5501/redirect-response-handler-test.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-new */
+import test from 'ava';
+import RedirectResponseHandler from '../../../../src/response-handlers/redirect-response-handler';
+import RedirectResponse from '../../../../src/responses/redirect-response';
+import checkNotNullTest from '../../../helpers/check-not-null.js';
+import User from '../../../../src/user';
+import FirebaseServer from 'firebase-server';
+
+let server = null;
+
+test.before(() => {
+  server = new FirebaseServer(5501, 'localhost.firebaseio.test', {
+    users: {
+      cli_2: { menuLocation: 'something' },
+    },
+  });
+});
+
+test.after.always('guaranteed cleanup', () => {
+  server.close();
+});
+
+checkNotNullTest(['response', 'user'], (args) => { new RedirectResponseHandler(args); });
+
+test('can be constructed with default parameters', t => {
+  new RedirectResponseHandler({ response: {}, user: {} });
+  t.pass();
+});
+
+test.cb('updates user object', t => {
+  t.plan(2);
+
+  new User({ platformType: 'cli', platformId: 2 }).load().then((user) => {
+    t.is(user.state.menuLocation, 'something');
+
+    const response = new RedirectResponse({ path: 'default' });
+    const handler = new RedirectResponseHandler({ response, user });
+
+    handler.call(() => {
+      t.is(user.state.menuLocation, 'default');
+      t.end();
+    });
+  });
+});
---
+++ test/response-handlers/db-response-handlers/5502/user-state-response-handler-test.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-new */
+import test from 'ava';
+import UserStateResponseHandler from '../../../../src/response-handlers/user-state-response-handler'; // eslint-disable-line max-len
+import UserStateResponse from '../../../../src/responses/user-state-response';
+import checkNotNullTest from '../../../helpers/check-not-null.js';
+import User from '../../../../src/user';
+import FirebaseServer from 'firebase-server';
+
+let server = null;
+
+test.before(() => {
+  server = new FirebaseServer(5502, 'localhost.firebaseio.test', {
+    users: {
+      cli_1: { foo: 1, bar: 2 },
+    },
+  });
+});
+
+test.after.always('guaranteed cleanup', () => {
+  server.close();
+});
+
+checkNotNullTest(['response', 'user'], (args) => { new UserStateResponseHandler(args); });
+
+test('can be constructed with default parameters', t => {
+  new UserStateResponseHandler({ response: {}, user: {} });
+  t.pass();
+});
+
+test.cb('updates user object', t => {
+  t.plan(4);
+
+  new User({ platformType: 'cli', platformId: 1 }).load().then((user) => {
+    t.is(user.state.foo, 1);
+    t.is(user.state.bar, 2);
+
+    const response = new UserStateResponse({ foo: 123 });
+    const handler = new UserStateResponseHandler({ response, user });
+
+    handler.call(() => {
+      t.is(user.state.foo, 123);
+      t.is(user.state.bar, 2);
+      t.end();
+    });
+  });
+});
