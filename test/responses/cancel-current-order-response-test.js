37d8ab040d54ce92eba584321be834ad822848fa
Roman Pushkin <roman.pushkin@gmail.com>
2016-08-27 22:24:14 -0700

    #208 Simplify cancel current order response

--- test/responses/cancel-current-order-response-test.js
+++ test/responses/cancel-current-order-response-test.js
@@ -1,17 +1,13 @@
 /* eslint-disable no-new */
 import test from 'ava';
 import CancelCurrentOrderResponse from '../../src/responses/cancel-current-order-response';
-import checkNotNullTest from '../helpers/check-not-null.js';
-
-checkNotNullTest('passengerKey', (args) => { new CancelCurrentOrderResponse(args); });
 
 test('can be constructed with default parameters', t => {
-  new CancelCurrentOrderResponse({ passengerKey: 'telegram_31337' });
+  new CancelCurrentOrderResponse();
   t.pass();
 });
 
-test('should save properties and have \'user-state\' type', t => {
-  const r = new CancelCurrentOrderResponse({ passengerKey: 'telegram_31337' });
+test('should have type', t => {
+  const r = new CancelCurrentOrderResponse();
   t.is(r.type, 'cancel-current-order');
-  t.is(r.passengerKey, 'telegram_31337');
 });
