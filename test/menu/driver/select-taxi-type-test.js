a9a79508e1886d8c3ed9db86503c94ebe9dd2caa
Roman Pushkin <roman.pushkin@gmail.com>
2016-08-30 20:51:46 -0700

    #208 Ask for driver's vehicle type

---
+++ test/menu/driver/select-taxi-type-test.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-new, no-unused-vars */
+import test from 'ava';
+import routes from '../../../src/routes'; // to aviod circular dependencies
+import SelectTaxiType from '../../../src/actions/menu/driver/select-taxi-type';
+import { i18n } from '../../spec-support';
+
+const user = {};
+
+test('can be constructed with default parameters', t => {
+  const action = new SelectTaxiType({ i18n, user });
+  t.is(action.type, 'driver-select-taxi-type');
+  t.pass();
+});
+
+test('should return composite response on get', t => {
+  const action = new SelectTaxiType({ i18n, user });
+  const response = action.get();
+  t.is(response.type, 'composite');
+  t.is(response.responses[0].type, 'text');
+  t.is(response.responses[0].message, i18n.__('driver-select-taxi-type.select_taxi_type'));
+  t.is(response.responses[1].type, 'options');
+  t.is(response.responses[1].rows[0][0].value, 'scooter');
+  t.is(response.responses[1].rows[0][1].value, 'bike');
+  t.is(response.responses[1].rows[0][2].value, 'car');
+  t.is(response.responses[1].rows[0][0].label, i18n.__('driver-select-taxi-type.scooter'));
+  t.is(response.responses[1].rows[0][1].label, i18n.__('driver-select-taxi-type.bike'));
+  t.is(response.responses[1].rows[0][2].label, i18n.__('driver-select-taxi-type.car'));
+});
+
+test('should return composite response on post for scooter, bike, and car', t => {
+  const reactions = ['scooter', 'bike', 'car'];
+  const action = new SelectTaxiType({ i18n, user });
+
+  for (const reaction of reactions) {
+    const response = action.post(reaction);
+    t.is(response.type, 'composite');
+    t.is(response.responses[0].type, 'text');
+    t.is(response.responses[0].message, '👌 OK!');
+    t.is(response.responses[1].type, 'user-state');
+    t.is(response.responses[1].state.taxiType, reaction);
+    t.is(response.responses[2].type, 'redirect');
+    t.is(response.responses[2].path, 'foo');
+  }
+});
+
+test.cb('should throw error on post for unknown reaction', t => {
+  const action = new SelectTaxiType({ i18n, user });
+  const err = 'unsupported choice \'whatever\'';
+
+  t.plan(1);
+  t.throws(() => { action.post('whatever'); }, err);
+  t.end();
+});
