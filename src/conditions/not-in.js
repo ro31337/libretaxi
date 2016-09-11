e0c901d062db400b8206c598ba6460645aead804
Roman Pushkin <roman.pushkin@gmail.com>
2016-09-11 11:00:22 -0700

    #236 Add NotIn condition

---
+++ src/conditions/not-in.js
@@ -0,0 +1,29 @@
+import Condition from './condition';
+
+/**
+ * Not In Condition.
+ * Accepts value and array. Returns true if value is not in array.
+ *
+ * @author Roman Pushkin (roman.pushkin@gmail.com)
+ * @date 2016-09-11
+ * @extends {Condition}
+ * @version 1.1
+ * @since 0.1.0
+ */
+export default class NotIn extends Condition {
+  constructor(value, arr) {
+    super();
+    this.value = value;
+    this.arr = arr;
+  }
+
+  /**
+   * Entry point for condition.
+   *
+   * @returns {boolean} result - returns `true` if actual equals expected, otherwise
+   * returns `false`.
+   */
+  call() {
+    return !this.arr.includes(this.value);
+  }
+}
