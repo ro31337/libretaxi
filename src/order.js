dcc4eb47b8b05aa026b77b20bf2fb0c4b2db9bf7
Roman Pushkin <roman.pushkin@gmail.com>
2016-12-07 19:32:43 -0800

    #362 Don't inform driver about the same order twice

--- src/order.js
+++ src/order.js
@@ -13,6 +13,7 @@ import checkNotNull from './validations/check-not-null';
  * - driver key
  * - createdAt
  * - acceptedAt
+ * - notified
  * and so on...
  * ...plus geo position (to be set with geoFire package).
  *
@@ -42,5 +43,25 @@ export default class Order extends
       key: this.orderKey,
       table: 'orders',
     };
+  }
+
+  /**
+   * Add user to notified list. Note: you probably want to `.save()` these changes.
+   *
+   * @param {String} userKey - user key to add to notified list
+   */
+  markNotified(userKey) {
+    if (!this.state.notified) { this.setState({ notified: [] }); }
+    this.state.notified.push(userKey);
+  }
+
+  /**
+   * Check if the user is in notified list for this order.
+   *
+   * @param {String} userKey - user key to check
+   * @return {boolean} result - true if user was already notified, otherwise false
+   */
+  isNotified(userKey) {
+    return ((this.state || {}).notified || []).includes(userKey);
   }
 }
--- test/response-handlers/support/notify-driver-test.js
+++ test/response-handlers/support/notify-driver-test.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-new */
+/* eslint-disable no-new, max-len, arrow-body-style, no-param-reassign */
 import test from 'ava';
 import NotifyDriver from '../../../src/response-handlers/support/notify-driver';
 import { ss } from '../../spec-support';
@@ -12,11 +12,12 @@ const loadUser = (user) => {
   const then = (f) => {
     f(user);
   };
-  return () => { return { then }; }; // eslint-disable-line arrow-body-style
+  return () => { return { then }; };
 };
 
 const shouldFail = (t, failReason, user, order) => {
   t.plan(1);
+  if (!order.isNotified) order.isNotified = () => { return false; };
   const successCallback = () => { t.fail(); };
   const failCallback = (reason) => { t.is(reason, failReason); t.end(); };
   const subject = new NotifyDriver({ successCallback, failCallback, loadUser: loadUser(user) });
@@ -33,6 +34,16 @@ test.cb('should not notify driver when order is stale', t => {
     'order is stale',
     {},
     { state: { status: 'new', createdAt: (new Date).getTime() - 15 * 60 * 1000 - 1 } }
+  );
+});
+
+test.cb('should not notify driver when driver was notified already', t => {
+  const isNotified = () => { return true; };
+  shouldFail(
+    t,
+    'driver was already notified',
+    {},
+    { state: { status: 'new', createdAt: (new Date).getTime() }, isNotified }
   );
 });
 
@@ -73,7 +84,9 @@ test.cb('should not notify driver when driver is busy', t => {
 });
 
 test.cb('should notify driver when matched', t => {
-  t.plan(1);
+  t.plan(3);
+  const saveSpy = ss.sinon.spy();
+  const markNotifiedSpy = ss.sinon.spy();
   const queue = { create: ss.sinon.spy() };
   const successCallback = () => {
     t.truthy(queue.create.calledWith({
@@ -88,6 +101,8 @@ test.cb('should notify driver when matched', t => {
       },
       route: 'driver-order-new',
     }));
+    t.truthy(saveSpy.calledWith());
+    t.truthy(markNotifiedSpy.calledWith('cli_1'));
     t.end();
   };
   const failCallback = () => { t.fail(); };
@@ -102,6 +117,9 @@ test.cb('should notify driver when matched', t => {
       passengerKey: 'cli_123',
       createdAt: (new Date).getTime(),
     },
+    save: saveSpy,
+    isNotified: () => { return false; },
+    markNotified: markNotifiedSpy,
   };
   const user = { state: { userType: 'driver', vehicleType: 'car', menuLocation: 'driver-index' } };
   const subject = new NotifyDriver({
