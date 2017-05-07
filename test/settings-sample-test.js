279fc6fa31b9e28e0cef06dea2a7631ebf414eec
Roman Pushkin <roman.pushkin@gmail.com>
2017-05-07 16:55:55 -0700

    #548 Update docs and add settings tests

--- test/settings-sample-test.js
+++ test/settings-sample-test.js
@@ -1,7 +1,8 @@
 import test from 'ava';
 import Settings from '../settings-sample';
 
-test('sample settings should have empty telegram token', t => {
+test('sample settings should have some empty tokens/keys', t => {
   const settings = new Settings();
   t.is(settings.TELEGRAM_TOKEN, '');
+  t.is(settings.GEOCODING_API_KEY, '');
 });
