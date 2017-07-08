afbef7c9368b685c2cc09fcbcd0e0e3846c4aace
Roman Pushkin <roman.pushkin@gmail.com>
2017-07-07 20:00:15 -0700

    #497 Long Live Freedom (#584)

--- src/support/history-hash.js
+++ src/support/history-hash.js
@@ -1,3 +1,21 @@
+/*
+    LibreTaxi, free and open source ride sharing platform.
+    Copyright (C) 2016-2017  Roman Pushkin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /**
  * Naive implementation of LRU-like cache. This class allows to keep up to `capacity` number
  * of items in hash. Older items are removed from hash. "Older item" is item that has not
