40cf3e2a1e74750e71a7d73d8cba1d2c73f349ad
Roman Pushkin <roman.pushkin@gmail.com>
2017-08-13 18:40:30 -0700

    #587 Include license text for tests

--- test/response-handlers/if-response-handler-test.js
+++ test/response-handlers/if-response-handler-test.js
@@ -1,3 +1,21 @@
+/*
+    LibreTaxi, free and open source ride sharing platform.
+    Copyright (C) 2016-2017  Roman Pushkin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /* eslint-disable no-new, no-console */
 import test from 'ava';
 import IfResponseHandler from '../../src/response-handlers/if-response-handler';
