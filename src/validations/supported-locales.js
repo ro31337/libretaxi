9dd0f73ab07db70a3a29beaf6967690bd53aff5e
Abu Zafar <azmikbal@gmail.com>
2019-12-07 22:47:16 +0600

    Updating Bangla locales

--- src/validations/supported-locales.js
+++ src/validations/supported-locales.js
@@ -54,6 +54,7 @@ localeMap.set('vi', '🇻🇳 Tiếng Việt');
 localeMap.set('ki', 'Kiribati');
 localeMap.set('ku', 'کوردی 🇹🇯');
 localeMap.set('ar', 'عربي');
+localeMap.set('bn', 'বাংলা');
 
 /**
  * @typedef SupportedLocales
